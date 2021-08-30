b90be8730e1d29f02878795114643040b66e6dd5
Miquel <76565907+mpieb@users.noreply.github.com>
2021-08-30 17:02:31 +0200

    Add files via upload

---
+++ gradle_exercici/src/main/java/com/example/gradle_exercici/GradleExerciciApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle_exercici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleExerciciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleExerciciApplication.class, args);
+	}
+
+}
---
+++ gradle_exercici/src/test/java/com/example/gradle_exercici/GradleExerciciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradle_exercici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleExerciciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
